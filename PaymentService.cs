8b99ad99d2f5b7e64c6576b17a3ea3ea06505cdf
Ahmad Moussawi <aymoussawi@gmail.com>
2019-07-23 09:52:22 +0300

    Adding multiapproval on payment, WOP

--- PaymentService.cs
+++ PaymentService.cs
@@ -4,13 +4,19 @@ namespace App.Services
     {
 
         private readonly AppDbContext db;
-        public PaymentService(AppDbContext db)
+        public PaymentService(AppDbContext db, MultiapprovalService multiapproval)
         {
             this.db = db;
+            this.multiapproval = multiapproval;
         }
 
         public Payment Create(double amount, string description)
         {
+            if(! this.multiapproval.CanCreate<Payment>())
+            {
+                throw new InvalidOperationException("You can't create a payment");
+            }
+
             var payment = new Payment{
                 Amount = amount,
                 Currency = description
@@ -18,6 +24,8 @@ namespace App.Services
 
             db.Payments.Add(payment);
 
+            this.multiapproval.QueueForApproval(payment);
+`
             db.SaveChanges();
 
             return Payment;
